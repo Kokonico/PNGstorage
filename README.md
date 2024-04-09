c4f5c55b9a79d641c9b478b876b5c02c146729e9
Kokonico <kokonico@duck.com>
2024-04-09 15:03:01 -0700

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# PNGstorage
+
+encrypt files into PNG images that appear to be white noise.
+
+## Usage
+
+```bash
+poetry install
+
+poetry run python PNGstorage.py
+```
+
+follow the instructions in the terminal.
---
+++ main.py
@@ -0,0 +1,84 @@
+"""any file to PNG"""
+import os
+from PIL import Image, PngImagePlugin
+import math
+
+
+# convert all bytes to pixels, and save as PNG
+
+def encode():
+    """main"""
+    # read file
+    file = input(f"Select File (current path: {os.getcwd()}): ")
+
+    # get a file extension
+    _, file_extension = os.path.splitext(file)
+
+    # read file as bytes
+    with open(file, 'rb') as f:
+        byte_data = f.read()
+
+    # Calculate the padding needed
+    padding = len(byte_data) % 3
+
+    # Add the padding to the byte data
+    byte_data += b'\x00' * padding
+
+    # Calculate total pixels and find width and height
+    total_pixels = len(byte_data) // 3
+    sqrt_pixels = math.isqrt(total_pixels)  # integer square root
+
+    # If total_pixels is a perfect square
+    if sqrt_pixels * sqrt_pixels == total_pixels:
+        width = height = sqrt_pixels
+    else:
+        # Find the largest factor of total_pixels near to sqrt_pixels
+        for factor in range(sqrt_pixels, 0, -1):
+            if total_pixels % factor == 0:
+                width, height = factor, total_pixels // factor
+                break
+
+    # Convert the bytes to an image
+    image = Image.frombytes('RGB', (width, height), byte_data)
+
+    # Create a PngInfo object and add a file type to it
+    pnginfo = PngImagePlugin.PngInfo()
+    pnginfo.add_text('file_type', file_extension)
+
+    # Save the new image
+    image.save('output.png', pnginfo=pnginfo)
+
+    print("Done!")
+
+
+def decode():
+    # Open the image
+    image = Image.open(input("Select Image: "))
+
+    # Get the file type from image info
+    try:
+        file_type = image.text.get('file_type', '')
+    except AttributeError:
+        print("Unable to get file type from image info, you may need to manually add the file extension")
+        file_type = ''
+
+    # Convert the image back to bytes
+    byte_data = image.tobytes()
+
+    # Write the bytes back to a file
+    with open(f'original_file{file_type}', 'wb') as f:
+        f.write(byte_data)
+
+
+if __name__ == "__main__":
+    while True:
+        choice = input("1. Encode\n2. Decode\n3. Exit\nChoice: ")
+        match choice:
+            case '1':
+                encode()
+            case '2':
+                decode()
+            case '3':
+                exit(0)
+            case _:
+                print("Invalid Choice")
